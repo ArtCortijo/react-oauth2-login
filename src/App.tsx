1e1384f75117167d1461c7032fc3aa7f88b9786f
acortijo84 <artcortijo@gmail.com>
2025-03-06 21:02:49 -0500

    Adding login, logout buttons and getting Profile data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
-import { GoogleLogin } from '@react-oauth/google';
+import { useState, useEffect } from 'react';
+import { GoogleLogin, googleLogout, useGoogleLogin } from '@react-oauth/google';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import axios from 'axios';
+
+interface UserResponse {
+	access_token: string;
+	[key: string]: any;
+}
+
+interface ProfileData {
+	name: string;
+	email: string;
+	picture: string;
+	[key: string]: any;
+}
 
 function App() {
+	const [user, setUser] = useState<UserResponse | null>(null);
+	const [profile, setProfile] = useState<ProfileData | null>(null);
+
+	const logIn = useGoogleLogin({
+		onSuccess: (codeResponse) => setUser(codeResponse),
+		onError: (error) => console.log('Login Failed:', error),
+	});
+
+	const logOut = (): void => {
+		googleLogout();
+		setProfile(null);
+	};
+
+	useEffect(() => {
+		if (user && user.access_token) {
+			axios
+				.get<ProfileData>(
+					`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${user.access_token}`,
+					{
+						headers: {
+							Authorization: `Bearer ${user.access_token}`,
+							Accept: 'application/json',
+						},
+					}
+				)
+				.then((response) => setProfile(response.data))
+				.catch((error) => console.log(error));
+		}
+		return () => {
+			setUser(null);
+			setProfile(null);
+		};
+	}, [user]);
+
 	return (
 		<>
 			<div>
@@ -15,14 +63,32 @@ function App() {
 				</a>
 			</div>
 			<h1>React Google Login</h1>
-			<div className='google-login'>
+			{profile ? (
+				<div>
+					<img src={profile.picture} alt={profile.name} />
+					<h3>User logged in</h3>
+					<p>Name: {profile.name}</p>
+					<p>Email: {profile.email}</p>
+					<button className='logout-btn' onClick={logOut}>
+						Logout
+					</button>
+				</div>
+			) : (
+				<div>
+					<button className='login-btn' onClick={() => logIn()}>
+						Login with Google
+					</button>
+				</div>
+			)}
+
+			{/* <div className='google-login'>
 				<GoogleLogin
 					onSuccess={(credentialResponse) => console.log(credentialResponse)}
 					onError={() => {
 						console.log('Login Failed');
 					}}
 				/>
-			</div>
+			</div> */}
 			<div className='card'>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
